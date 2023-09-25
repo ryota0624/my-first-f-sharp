1796ca25f993147daed1575ae45a83f1275dc2cc
ryota.suzuki <ryota.suzuki@10x.co.jp>
2023-09-25 22:02:29 +0900

    first commit

---
+++ Program.fs
@@ -0,0 +1,21 @@
+﻿open System;
+
+type UserId = UserId of string
+type ConstructedUser(id: UserId) = 
+    member this.toStr() = id
+
+type User = NotRegistered | Registered of ConstructedUser
+
+let printUser user = 
+    match user with
+    | NotRegistered -> printfn "iam not registered"
+    | Registered constructedUser -> printfn $"iam constructed user ({constructedUser.toStr()})"
+
+
+// For more information see https://aka.ms/fsharp-console-apps
+printUser User.NotRegistered
+Guid.NewGuid().ToString() 
+    |> UserId
+    |> ConstructedUser 
+    |> Registered 
+    |> printUser 
